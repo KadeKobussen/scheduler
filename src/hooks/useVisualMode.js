bd235e441d963246c5e3c8ce3660e7072baa881c
Kade Kobussen <kadekobussen50@gmail.com>
2023-04-21 14:00:07 -0700

    build custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newHistory = [...history.slice(0, history.length - 1)];
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
