f2cee27a132780ab614c101f1ffe38bb508e84df
Kade Kobussen <kadekobussen50@gmail.com>
2023-04-25 22:53:47 -0700

    finish adding all tests to Application.test.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,7 @@
 import React from "react";
+import axios from "axios";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, getByDisplayValue } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -67,6 +68,69 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
   expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
 
   debug();
+});
+
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+// 1. Render the Application.
+const { container, debug } = render(<Application />);
+// 2. Wait until the text "Archie Cohen" is displayed.
+await waitForElement(() => getByText(container, "Archie Cohen"));
+// 3. Click the "Edit" button on the booked appointment.
+const appointment = getAllByTestId(container, "appointment").find(
+  appointment => queryByText(appointment, "Archie Cohen")
+);
+fireEvent.click(getByAltText(appointment, "Edit"));
+// 4. enter name field.
+const input = getByDisplayValue(appointment, "Archie Cohen");
+fireEvent.click(input);
+// 5.change name.
+fireEvent.change(input, { target: { value: "Kade Kobussen" } });
+// 6.click save
+fireEvent.click(getByText(appointment, "Save"));
+// 7.verify spots remaining are the same 
+await waitForElement(() => getByText(container, "Monday"));
+const day = getAllByTestId(container, "day").find(day =>
+  queryByText(day, "Monday")
+);
+expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+});
+
+it("shows the save error when failing to save an appointment", async () => {
+  axios.put.mockRejectedValueOnce();
+
+  const { container, debug } = render(<Application />);
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointment = getAllByTestId(container, "appointment")[0];
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" },
+  });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
+  await waitForElement(() =>
+    getByText(appointment, "Could not create appointment")
+  );
+})
+
+it("shows the delete error when failing to delete an existing appointment", async () => {
+  axios.delete.mockRejectedValueOnce();
+
+  const { container } = render(<Application />);
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointment = getAllByTestId(container, "appointment").find(
+    (appointment) => queryByText(appointment, "Archie Cohen")
+  );
+  fireEvent.click(getByAltText(appointment, "Delete"));
+  expect(getByText(appointment, /Delete the appointment/i)).toBeInTheDocument();
+  fireEvent.click(getByText(appointment, "Confirm"));
+
+  await waitForElement(() =>
+    getByText(appointment, "Could not cancel appointment")
+  );
 });
 
 
